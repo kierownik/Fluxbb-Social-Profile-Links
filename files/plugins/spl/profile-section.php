d7aa00649c25de80227828f0344a324a6d9e068b
Daniël Rokven <rokven@gmail.com>
2013-06-15 23:49:49 +0200

    Updated install.txt to show the good lines and added some comma's
    Now also when the admin desided that if he does not want github to be
    shown that the users also do not see it in profile.

--- files/plugins/spl/profile-section.php
+++ files/plugins/spl/profile-section.php
@@ -15,12 +15,32 @@
 					<fieldset>
 						<legend>Enter your usernames</legend>
 						<div class="infldset">
-							<input type="hidden" name="form_sent" value="1" />
-							<label>Github<br /><input id="spl_github" type="text" name="form[spl_github]" value="<?php echo pun_htmlspecialchars($user['spl_github']) ?>" size="40" maxlength="75" placeholder="Username" /><br /></label>
-							<label>Facebook<br /><input id="spl_facebook" type="text" name="form[spl_facebook]" value="<?php echo $user['spl_facebook'] ?>" size="40" maxlength="75" placeholder="Username" /><br /></label>
-							<label>Twitter<br /><input id="spl_twitter" type="text" name="form[spl_twitter]" value="<?php echo pun_htmlspecialchars($user['spl_twitter']) ?>" size="40" maxlength="75" placeholder="Username" /><br /></label>
-							<label>Youtube<br /><input id="spl_youtube" type="text" name="form[spl_youtube]" value="<?php echo pun_htmlspecialchars($user['spl_youtube']) ?>" size="40" maxlength="75" placeholder="Username" /><br /></label>
-							<label>Google+<br /><input id="spl_googleplus" type="text" name="form[spl_googleplus]" value="<?php echo pun_htmlspecialchars($user['spl_googleplus']) ?>" size="40" maxlength="75" placeholder="google+ user id"/><br /></label>
+              <input type="hidden" name="form_sent" value="1" />
+            <?php
+            if ($pun_config['o_spl_view_github'] == '1' OR $pun_config['o_spl_prof_github'] == '1')
+            {
+							echo '<label>Github<br /><input id="spl_github" type="text" name="form[spl_github]" value="'.pun_htmlspecialchars($user['spl_github']).'" size="40" maxlength="75" placeholder="Username" /><br /></label>';
+            }
+
+            if ($pun_config['o_spl_view_facebook'] == '1' OR $pun_config['o_spl_prof_facebook'] == '1')
+            {
+							echo '<label>Facebook<br /><input id="spl_facebook" type="text" name="form[spl_facebook]" value="'.$user['spl_facebook'].'" size="40" maxlength="75" placeholder="Username" /><br /></label>';
+            }
+
+            if ($pun_config['o_spl_view_twitter'] == '1' OR $pun_config['o_spl_prof_twitter'] == '1')
+            {
+							echo '<label>Twitter<br /><input id="spl_twitter" type="text" name="form[spl_twitter]" value="'.pun_htmlspecialchars($user['spl_twitter']).'" size="40" maxlength="75" placeholder="Username" /><br /></label>';
+            }
+
+            if ($pun_config['o_spl_view_youtube'] == '1' OR $pun_config['o_spl_prof_youtube'] == '1')
+            {
+							echo '<label>Youtube<br /><input id="spl_youtube" type="text" name="form[spl_youtube]" value="'.pun_htmlspecialchars($user['spl_youtube']).'" size="40" maxlength="75" placeholder="Username" /><br /></label>';
+            }
+
+            if ($pun_config['o_spl_view_googleplus'] == '1' OR $pun_config['o_spl_prof_googleplus'] == '1')
+            {
+							echo '<label>Google+<br /><input id="spl_googleplus" type="text" name="form[spl_googleplus]" value="'.pun_htmlspecialchars($user['spl_googleplus']).'" size="40" maxlength="75" placeholder="google+ user id"/><br /></label>';
+            } ?>
 						</div>
 					</fieldset>
 				</div>
