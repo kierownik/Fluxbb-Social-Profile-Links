fa63d2a575f290a889950a7b76d31dad578a6991
Daniël Rokven <rokven@gmail.com>
2013-06-16 22:48:40 +0200

    change the tabs to spaces :)

--- files/plugins/spl/profile-section.php
+++ files/plugins/spl/profile-section.php
@@ -1,53 +1,53 @@
 ﻿<?php
 
-		$page_title = array(pun_htmlspecialchars($pun_config['o_board_title']), $lang_common['Profile'], 'Social Profile Links');
-		define('PUN_ACTIVE_PAGE', 'profile');
-		require PUN_ROOT.'header.php';
+    $page_title = array(pun_htmlspecialchars($pun_config['o_board_title']), $lang_common['Profile'], 'Social Profile Links');
+    define('PUN_ACTIVE_PAGE', 'profile');
+    require PUN_ROOT.'header.php';
 
-		generate_profile_menu('spl');
+    generate_profile_menu('spl');
 
   // Load the social-profile-links.php language file
   require PUN_ROOT.'lang/'.$pun_user['language'].'/social-profile-links.php';
 
 ?>
-	<div class="blockform">
-		<h2><span><?php echo pun_htmlspecialchars($user['username']).' - Social Profile Links' ?></span></h2>
-		<div class="box">
-			<form id="profile3a" method="post" action="profile.php?section=spl&amp;id=<?php echo $id ?>">
-				<div class="inform">
-					<fieldset>
-						<legend><?php echo $lang_spl['username of user id']; ?></legend>
-						<div class="infldset">
+  <div class="blockform">
+    <h2><span><?php echo pun_htmlspecialchars($user['username']).' - Social Profile Links' ?></span></h2>
+    <div class="box">
+      <form id="profile3a" method="post" action="profile.php?section=spl&amp;id=<?php echo $id ?>">
+        <div class="inform">
+          <fieldset>
+            <legend><?php echo $lang_spl['username of user id']; ?></legend>
+            <div class="infldset">
               <input type="hidden" name="form_sent" value="1" />
             <?php
             if ($pun_config['o_spl_view_github'] == '1' OR $pun_config['o_spl_prof_github'] == '1')
             {
-							echo '<label>'.$lang_spl['github'].'<br /><input id="spl_github" type="text" name="form[spl_github]" value="'.pun_htmlspecialchars($user['spl_github']).'" size="40" maxlength="75" placeholder="'.$lang_spl['username'].'" /><br /></label>';
+              echo '<label>'.$lang_spl['github'].'<br /><input id="spl_github" type="text" name="form[spl_github]" value="'.pun_htmlspecialchars($user['spl_github']).'" size="40" maxlength="75" placeholder="'.$lang_spl['username'].'" /><br /></label>';
             }
 
             if ($pun_config['o_spl_view_facebook'] == '1' OR $pun_config['o_spl_prof_facebook'] == '1')
             {
-							echo '<label>'.$lang_spl['facebook'].'<br /><input id="spl_facebook" type="text" name="form[spl_facebook]" value="'.$user['spl_facebook'].'" size="40" maxlength="75" placeholder="'.$lang_spl['username'].'" /><br /></label>';
+              echo '<label>'.$lang_spl['facebook'].'<br /><input id="spl_facebook" type="text" name="form[spl_facebook]" value="'.$user['spl_facebook'].'" size="40" maxlength="75" placeholder="'.$lang_spl['username'].'" /><br /></label>';
             }
 
             if ($pun_config['o_spl_view_twitter'] == '1' OR $pun_config['o_spl_prof_twitter'] == '1')
             {
-							echo '<label>'.$lang_spl['twitter'].'<br /><input id="spl_twitter" type="text" name="form[spl_twitter]" value="'.pun_htmlspecialchars($user['spl_twitter']).'" size="40" maxlength="75" placeholder="'.$lang_spl['username'].'" /><br /></label>';
+              echo '<label>'.$lang_spl['twitter'].'<br /><input id="spl_twitter" type="text" name="form[spl_twitter]" value="'.pun_htmlspecialchars($user['spl_twitter']).'" size="40" maxlength="75" placeholder="'.$lang_spl['username'].'" /><br /></label>';
             }
 
             if ($pun_config['o_spl_view_youtube'] == '1' OR $pun_config['o_spl_prof_youtube'] == '1')
             {
-							echo '<label>'.$lang_spl['youtube'].'<br /><input id="spl_youtube" type="text" name="form[spl_youtube]" value="'.pun_htmlspecialchars($user['spl_youtube']).'" size="40" maxlength="75" placeholder="'.$lang_spl['username'].'" /><br /></label>';
+              echo '<label>'.$lang_spl['youtube'].'<br /><input id="spl_youtube" type="text" name="form[spl_youtube]" value="'.pun_htmlspecialchars($user['spl_youtube']).'" size="40" maxlength="75" placeholder="'.$lang_spl['username'].'" /><br /></label>';
             }
 
             if ($pun_config['o_spl_view_googleplus'] == '1' OR $pun_config['o_spl_prof_googleplus'] == '1')
             {
-							echo '<label>'.$lang_spl['google+'].'<br /><input id="spl_googleplus" type="text" name="form[spl_googleplus]" value="'.pun_htmlspecialchars($user['spl_googleplus']).'" size="40" maxlength="75" placeholder="'.$lang_spl['user id'].'"/><br /></label>';
+              echo '<label>'.$lang_spl['google+'].'<br /><input id="spl_googleplus" type="text" name="form[spl_googleplus]" value="'.pun_htmlspecialchars($user['spl_googleplus']).'" size="40" maxlength="75" placeholder="'.$lang_spl['user id'].'"/><br /></label>';
             } ?>
-						</div>
-					</fieldset>
-				</div>
-				<p class="buttons"><input type="submit" name="update" value="<?php echo $lang_common['Submit'] ?>" /> <?php echo $lang_profile['Instructions'] ?></p>
-			</form>
-		</div>
-	</div>
+            </div>
+          </fieldset>
+        </div>
+        <p class="buttons"><input type="submit" name="update" value="<?php echo $lang_common['Submit'] ?>" /> <?php echo $lang_profile['Instructions'] ?></p>
+      </form>
+    </div>
+  </div>
--- files/plugins/spl/profile.php
+++ files/plugins/spl/profile.php
@@ -4,79 +4,79 @@ if (!$pun_user['is_guest'])
   // Load the social-profile-links.php language file
   require PUN_ROOT.'lang/'.$pun_user['language'].'/social-profile-links.php';
 
-	if ($user['spl_github'] != '' AND $pun_config['o_spl_prof_github'] == '1')
-	{
-		$user['spl_github'] = pun_htmlspecialchars(($pun_config['o_censoring'] == '1') ? censor_words($user['spl_github']) : $user['spl_github']);
-		$user_personal[] = '<dt>'.$lang_spl['github'].'</dt>';
-		
-		if ($pun_config['o_spl_icon_github'] == '1')
-		{
-			$user_personal[] = '<dd><span><a href="https://github.com/'.$user['spl_github'].'" rel="nofollow"><img src="'.$pun_config['o_base_url'].'/img/spl/GitHub.png" /></a></span></dd>';
-		}
-		else
-		{
-			$user_personal[] = '<dd><span class="website"><a href="https://github.com/'.$user['spl_github'].'" rel="nofollow">'.$user['spl_github'].'</a></span></dd>';
-		}
-	}
+  if ($user['spl_github'] != '' AND $pun_config['o_spl_prof_github'] == '1')
+  {
+    $user['spl_github'] = pun_htmlspecialchars(($pun_config['o_censoring'] == '1') ? censor_words($user['spl_github']) : $user['spl_github']);
+    $user_personal[] = '<dt>'.$lang_spl['github'].'</dt>';
+    
+    if ($pun_config['o_spl_icon_github'] == '1')
+    {
+      $user_personal[] = '<dd><span><a href="https://github.com/'.$user['spl_github'].'" rel="nofollow"><img src="'.$pun_config['o_base_url'].'/img/spl/GitHub.png" /></a></span></dd>';
+    }
+    else
+    {
+      $user_personal[] = '<dd><span class="website"><a href="https://github.com/'.$user['spl_github'].'" rel="nofollow">'.$user['spl_github'].'</a></span></dd>';
+    }
+  }
 
-	if ($user['spl_facebook'] != '' AND $pun_config['o_spl_prof_facebook'] == '1')
-	{
-		$user['spl_facebook'] = pun_htmlspecialchars(($pun_config['o_censoring'] == '1') ? censor_words($user['spl_facebook']) : $user['spl_facebook']);
-		$user_personal[] = '<dt>'.$lang_spl['facebook'].'</dt>';
-		
-		if ($pun_config['o_spl_icon_facebook'] == '1')
-		{
-			$user_personal[] = '<dd><span><a href="https://facebook.com/'.$user['spl_facebook'].'" rel="nofollow"><img src="'.$pun_config['o_base_url'].'/img/spl/Facebook.png" /></a></span></dd>';
-		}
-		else
-		{
+  if ($user['spl_facebook'] != '' AND $pun_config['o_spl_prof_facebook'] == '1')
+  {
+    $user['spl_facebook'] = pun_htmlspecialchars(($pun_config['o_censoring'] == '1') ? censor_words($user['spl_facebook']) : $user['spl_facebook']);
+    $user_personal[] = '<dt>'.$lang_spl['facebook'].'</dt>';
+    
+    if ($pun_config['o_spl_icon_facebook'] == '1')
+    {
+      $user_personal[] = '<dd><span><a href="https://facebook.com/'.$user['spl_facebook'].'" rel="nofollow"><img src="'.$pun_config['o_base_url'].'/img/spl/Facebook.png" /></a></span></dd>';
+    }
+    else
+    {
       $user_personal[] = '<dd><span class="website"><a href="https://facebook.com/'.$user['spl_facebook'].'" rel="nofollow">'.$user['spl_facebook'].'</a></span></dd>';
     }
-	}
+  }
 
-	if ($user['spl_twitter'] != '' AND $pun_config['o_spl_prof_twitter'] == '1')
-	{
-		$user['spl_twitter'] = pun_htmlspecialchars(($pun_config['o_censoring'] == '1') ? censor_words($user['spl_twitter']) : $user['spl_twitter']);
-		$user_personal[] = '<dt>'.$lang_spl['twitter'].'</dt>';
-		
-		if ($pun_config['o_spl_icon_twitter'] == '1')
-		{
-			$user_personal[] = '<dd><span><a href="https://twitter.com/'.$user['spl_twitter'].'" rel="nofollow"><img src="'.$pun_config['o_base_url'].'/img/spl/Twitter.png" /></a></span></dd>';
-		}
-		else
-		{
+  if ($user['spl_twitter'] != '' AND $pun_config['o_spl_prof_twitter'] == '1')
+  {
+    $user['spl_twitter'] = pun_htmlspecialchars(($pun_config['o_censoring'] == '1') ? censor_words($user['spl_twitter']) : $user['spl_twitter']);
+    $user_personal[] = '<dt>'.$lang_spl['twitter'].'</dt>';
+    
+    if ($pun_config['o_spl_icon_twitter'] == '1')
+    {
+      $user_personal[] = '<dd><span><a href="https://twitter.com/'.$user['spl_twitter'].'" rel="nofollow"><img src="'.$pun_config['o_base_url'].'/img/spl/Twitter.png" /></a></span></dd>';
+    }
+    else
+    {
       $user_personal[] = '<dd><span class="website"><a href="https://twitter.com/'.$user['spl_twitter'].'" rel="nofollow">'.$user['spl_twitter'].'</a></span></dd>';
     }
-	}
+  }
 
-	if ($user['spl_youtube'] != '' AND $pun_config['o_spl_prof_youtube'] == '1')
-	{
-		$user['spl_youtube'] = pun_htmlspecialchars(($pun_config['o_censoring'] == '1') ? censor_words($user['spl_youtube']) : $user['spl_youtube']);
-		$user_personal[] = '<dt>'.$lang_spl['youtube'].'</dt>';
-		
-		if ($pun_config['o_spl_icon_youtube'] == '1')
-		{
-			$user_personal[] = '<dd><span><a href="https://youtube.com/user/'.$user['spl_youtube'].'" rel="nofollow"><img src="'.$pun_config['o_base_url'].'/img/spl/YouTube.png" /></a></span></dd>';
-		}
-		else
-		{
+  if ($user['spl_youtube'] != '' AND $pun_config['o_spl_prof_youtube'] == '1')
+  {
+    $user['spl_youtube'] = pun_htmlspecialchars(($pun_config['o_censoring'] == '1') ? censor_words($user['spl_youtube']) : $user['spl_youtube']);
+    $user_personal[] = '<dt>'.$lang_spl['youtube'].'</dt>';
+    
+    if ($pun_config['o_spl_icon_youtube'] == '1')
+    {
+      $user_personal[] = '<dd><span><a href="https://youtube.com/user/'.$user['spl_youtube'].'" rel="nofollow"><img src="'.$pun_config['o_base_url'].'/img/spl/YouTube.png" /></a></span></dd>';
+    }
+    else
+    {
       $user_personal[] = '<dd><span class="website"><a href="https://youtube.com/user/'.$user['spl_youtube'].'" rel="nofollow">'.$user['spl_youtube'].'</a></span></dd>';
     }
-	}
+  }
 
-	if ($user['spl_googleplus'] != '' AND $pun_config['o_spl_prof_googleplus'] == '1')
-	{
-		$user['spl_googleplus'] = pun_htmlspecialchars(($pun_config['o_censoring'] == '1') ? censor_words($user['spl_googleplus']) : $user['spl_googleplus']);
-		$user_personal[] = '<dt>'.$lang_spl['google+'].'</dt>';
-		
-		if ($pun_config['o_spl_icon_googleplus'] == '1')
-		{
-			$user_personal[] = '<dd><span><a href="https://plus.google.com/'.$user['spl_googleplus'].'/posts" rel="nofollow"><img src="img/spl/Google+.png" /></a></span></dd>';
-		}
-		else
-		{
+  if ($user['spl_googleplus'] != '' AND $pun_config['o_spl_prof_googleplus'] == '1')
+  {
+    $user['spl_googleplus'] = pun_htmlspecialchars(($pun_config['o_censoring'] == '1') ? censor_words($user['spl_googleplus']) : $user['spl_googleplus']);
+    $user_personal[] = '<dt>'.$lang_spl['google+'].'</dt>';
+    
+    if ($pun_config['o_spl_icon_googleplus'] == '1')
+    {
+      $user_personal[] = '<dd><span><a href="https://plus.google.com/'.$user['spl_googleplus'].'/posts" rel="nofollow"><img src="img/spl/Google+.png" /></a></span></dd>';
+    }
+    else
+    {
       $user_personal[] = '<dd><span class="website"><a href="https://plus.google.com/'.$user['spl_googleplus'].'/posts" rel="nofollow">'.$user['spl_googleplus'].'</a></span></dd>';
     }
-	}
+  }
 }
 ?>
