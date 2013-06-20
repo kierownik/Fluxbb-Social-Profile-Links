eaa123d6f5315c70885d8587ee7c494fdcd632d1
Daniël Rokven <rokven@gmail.com>
2013-06-20 16:09:44 +0200

    spl/viewtopic.php - if no links is provided, this is still being displayed.
    pun_htmlspecialchars should not be applied twice (during censoring and
    output).

--- files/plugins/spl/viewtopic.php
+++ files/plugins/spl/viewtopic.php
@@ -5,7 +5,11 @@ if ( $pun_config['o_spl_show_guest'] == '1' OR !$pun_user['is_guest'] )
   // Load the social-profile-links.php language file
   require PUN_ROOT.'lang/'.$pun_user['language'].'/social-profile-links.php';
 
-  $user_contacts[] = '<br /><br />';
+  // If there are links to display we need to add 2 empty newlines
+  if ( $pun_config['o_spl_view_github'] == '1' OR $pun_config['o_spl_view_facebook'] == '1' OR $pun_config['o_spl_view_twitter'] == '1' OR $pun_config['o_spl_view_youtube'] == '1' OR $pun_config['o_spl_view_googleplus'] == '1')
+  {
+    $user_contacts[] = '<br /><br />';
+  }
 
   if ( $pun_config['o_spl_view_github'] == '1' AND $cur_post['spl_github'] !='' )
   {
@@ -13,11 +17,11 @@ if ( $pun_config['o_spl_show_guest'] == '1' OR !$pun_user['is_guest'] )
 
     if ( $pun_config['o_spl_icon_github'] == '1' )
     {
-      $user_contacts[] = '<span><a href="https://github.com/'.pun_htmlspecialchars( $cur_post['spl_github'] ).'" rel="nofollow" title="'.$lang_spl['github'].'"><img src="'.$pun_config['o_base_url'].'/img/spl/GitHub.png" alt="" /></a></span>';
+      $user_contacts[] = '<span><a href="https://github.com/'. $cur_post['spl_github'].'" rel="nofollow" title="'.$lang_spl['github'].'"><img src="'.$pun_config['o_base_url'].'/img/spl/GitHub.png" alt="" /></a></span>';
     }
     else
     {
-      $user_contacts[] = '<span class="website"><a href="https://github.com/'.pun_htmlspecialchars( $cur_post['spl_github'] ).'" rel="nofollow">'.$lang_spl['github'].'</a></span>';
+      $user_contacts[] = '<span class="website"><a href="https://github.com/'.$cur_post['spl_github'].'" rel="nofollow">'.$lang_spl['github'].'</a></span>';
     }
   }
 
@@ -27,11 +31,11 @@ if ( $pun_config['o_spl_show_guest'] == '1' OR !$pun_user['is_guest'] )
 
     if ( $pun_config['o_spl_icon_facebook'] == '1' )
     {
-      $user_contacts[] = '<span><a href="https://www.facebook.com/'.pun_htmlspecialchars( $cur_post['spl_facebook'] ).'" rel="nofollow" title="'.$lang_spl['facebook'].'"><img src="'.$pun_config['o_base_url'].'/img/spl/Facebook.png" alt="" /></a></span>';
+      $user_contacts[] = '<span><a href="https://www.facebook.com/'.$cur_post['spl_facebook'].'" rel="nofollow" title="'.$lang_spl['facebook'].'"><img src="'.$pun_config['o_base_url'].'/img/spl/Facebook.png" alt="" /></a></span>';
     }
     else
     {
-      $user_contacts[] = '<span class="website"><a href="https://www.facebook.com/'.pun_htmlspecialchars( $cur_post['spl_facebook'] ).'" rel="nofollow">'.$lang_spl['facebook'].'</a></span>';
+      $user_contacts[] = '<span class="website"><a href="https://www.facebook.com/'.$cur_post['spl_facebook'].'" rel="nofollow">'.$lang_spl['facebook'].'</a></span>';
     }
   }
 
@@ -41,11 +45,11 @@ if ( $pun_config['o_spl_show_guest'] == '1' OR !$pun_user['is_guest'] )
 
     if ( $pun_config['o_spl_icon_twitter'] == '1' )
     {
-      $user_contacts[] = '<span><a href="https://twitter.com/'.pun_htmlspecialchars( $cur_post['spl_twitter'] ).'" rel="nofollow" title="'.$lang_spl['twitter'].'"><img src="'.$pun_config['o_base_url'].'/img/spl/Twitter.png" alt="" /></a></span>';
+      $user_contacts[] = '<span><a href="https://twitter.com/'.$cur_post['spl_twitter'].'" rel="nofollow" title="'.$lang_spl['twitter'].'"><img src="'.$pun_config['o_base_url'].'/img/spl/Twitter.png" alt="" /></a></span>';
     }
     else
     {
-      $user_contacts[] = '<span class="website"><a href="https://twitter.com/'.pun_htmlspecialchars( $cur_post['spl_twitter'] ).'" rel="nofollow">'.$lang_spl['twitter'].'</a></span>';
+      $user_contacts[] = '<span class="website"><a href="https://twitter.com/'.$cur_post['spl_twitter'].'" rel="nofollow">'.$lang_spl['twitter'].'</a></span>';
     }
   }
 
@@ -55,11 +59,11 @@ if ( $pun_config['o_spl_show_guest'] == '1' OR !$pun_user['is_guest'] )
 
     if ( $pun_config['o_spl_icon_youtube'] == '1' )
     {
-      $user_contacts[] = '<span><a href="https://youtube.com/user/'.pun_htmlspecialchars( $cur_post['spl_youtube'] ).'" rel="nofollow" title="'.$lang_spl['youtube'].'"><img src="'.$pun_config['o_base_url'].'/img/spl/YouTube.png" alt="" /></a></span>';
+      $user_contacts[] = '<span><a href="https://youtube.com/user/'.$cur_post['spl_youtube'].'" rel="nofollow" title="'.$lang_spl['youtube'].'"><img src="'.$pun_config['o_base_url'].'/img/spl/YouTube.png" alt="" /></a></span>';
     }
     else
     {
-      $user_contacts[] = '<span class="website"><a href="https://youtube.com/user/'.pun_htmlspecialchars( $cur_post['spl_youtube'] ).'" rel="nofollow">'.$lang_spl['youtube'].'</a></span>';
+      $user_contacts[] = '<span class="website"><a href="https://youtube.com/user/'.$cur_post['spl_youtube'].'" rel="nofollow">'.$lang_spl['youtube'].'</a></span>';
     }
   }
 
@@ -69,11 +73,11 @@ if ( $pun_config['o_spl_show_guest'] == '1' OR !$pun_user['is_guest'] )
 
     if ( $pun_config['o_spl_icon_googleplus'] == '1' )
     {
-      $user_contacts[] = '<span><a href="https://profiles.google.com/'.pun_htmlspecialchars( $cur_post['spl_googleplus'] ).'" rel="nofollow" title="'.$lang_spl['google+'].'"><img src="'.$pun_config['o_base_url'].'/img/spl/Google+.png" alt="" /></a></span>';
+      $user_contacts[] = '<span><a href="https://profiles.google.com/'.$cur_post['spl_googleplus'].'" rel="nofollow" title="'.$lang_spl['google+'].'"><img src="'.$pun_config['o_base_url'].'/img/spl/Google+.png" alt="" /></a></span>';
     }
     else
     {
-      $user_contacts[] = '<span class="website"><a href="https://profiles.google.com/'.pun_htmlspecialchars( $cur_post['spl_googleplus'] ).'" rel="nofollow">'.$lang_spl['google+'].'</a></span>';
+      $user_contacts[] = '<span class="website"><a href="https://profiles.google.com/'.$cur_post['spl_googleplus'].'" rel="nofollow">'.$lang_spl['google+'].'</a></span>';
     }
   }
 }
