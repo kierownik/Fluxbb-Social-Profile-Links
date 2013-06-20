9363a843d46bb179e68d1fc9c45506a6b6c62b83
Daniël Rokven <rokven@gmail.com>
2013-06-20 09:07:01 +0200

    Added censoring to spl/viewtopic.php

--- files/plugins/spl/viewtopic.php
+++ files/plugins/spl/viewtopic.php
@@ -8,6 +8,8 @@ if (!$pun_user['is_guest'])
 
   if ($pun_config['o_spl_view_github'] == '1' AND $cur_post['spl_github'] !='')
   {
+    $cur_post['spl_github'] = pun_htmlspecialchars(($pun_config['o_censoring'] == '1') ? censor_words($cur_post['spl_github']) : $cur_post['spl_github']);
+
     if ($pun_config['o_spl_icon_github'] == '1')
     {
       $user_contacts[] = '<span><a href="https://github.com/'.pun_htmlspecialchars($cur_post['spl_github']).'" rel="nofollow" title="'.$lang_spl['github'].'"><img src="'.$pun_config['o_base_url'].'/img/spl/GitHub.png" alt="" /></a></span>';
@@ -20,6 +22,8 @@ if (!$pun_user['is_guest'])
 
   if ($pun_config['o_spl_view_facebook'] == '1' AND $cur_post['spl_facebook'] !='')
   {
+    $cur_post['spl_facebook'] = pun_htmlspecialchars(($pun_config['o_censoring'] == '1') ? censor_words($cur_post['spl_facebook']) : $cur_post['spl_facebook']);
+
     if ($pun_config['o_spl_icon_facebook'] == '1')
     {
       $user_contacts[] = '<span><a href="https://www.facebook.com/'.pun_htmlspecialchars($cur_post['spl_facebook']).'" rel="nofollow" title="'.$lang_spl['facebook'].'"><img src="'.$pun_config['o_base_url'].'/img/spl/Facebook.png" alt="" /></a></span>';
@@ -32,6 +36,8 @@ if (!$pun_user['is_guest'])
 
   if ($pun_config['o_spl_view_twitter'] == '1' AND $cur_post['spl_twitter'] !='')
   {
+    $cur_post['spl_twitter'] = pun_htmlspecialchars(($pun_config['o_censoring'] == '1') ? censor_words($cur_post['spl_twitter']) : $cur_post['spl_twitter']);
+
     if ($pun_config['o_spl_icon_twitter'] == '1')
     {
       $user_contacts[] = '<span><a href="https://twitter.com/'.pun_htmlspecialchars($cur_post['spl_twitter']).'" rel="nofollow" title="'.$lang_spl['twitter'].'"><img src="'.$pun_config['o_base_url'].'/img/spl/Twitter.png" alt="" /></a></span>';
@@ -44,6 +50,8 @@ if (!$pun_user['is_guest'])
 
   if ($pun_config['o_spl_view_youtube'] == '1' AND $cur_post['spl_youtube'] !='')
   {
+    $cur_post['spl_youtube'] = pun_htmlspecialchars(($pun_config['o_censoring'] == '1') ? censor_words($cur_post['spl_youtube']) : $cur_post['spl_youtube']);
+
     if ($pun_config['o_spl_icon_youtube'] == '1')
     {
       $user_contacts[] = '<span><a href="https://youtube.com/user/'.pun_htmlspecialchars($cur_post['spl_youtube']).'" rel="nofollow" title="'.$lang_spl['youtube'].'"><img src="'.$pun_config['o_base_url'].'/img/spl/YouTube.png" alt="" /></a></span>';
@@ -56,6 +64,8 @@ if (!$pun_user['is_guest'])
 
   if ($pun_config['o_spl_view_googleplus'] == '1' AND $cur_post['spl_googleplus'] !='')
   {
+    $cur_post['spl_googleplus'] = pun_htmlspecialchars(($pun_config['o_censoring'] == '1') ? censor_words($cur_post['spl_googleplus']) : $cur_post['spl_googleplus']);
+
     if ($pun_config['o_spl_icon_googleplus'] == '1')
     {
       $user_contacts[] = '<span><a href="https://profiles.google.com/'.pun_htmlspecialchars($cur_post['spl_googleplus']).'" rel="nofollow" title="'.$lang_spl['google+'].'"><img src="'.$pun_config['o_base_url'].'/img/spl/Google+.png" alt="" /></a></span>';
