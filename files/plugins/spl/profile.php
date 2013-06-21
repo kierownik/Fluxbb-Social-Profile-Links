1f64a07c85b85387c506ddecea7c4815b4bb85cd
Daniël Rokven <rokven@gmail.com>
2013-06-21 22:25:45 +0200

    Forgot spl/profile.php :)

--- files/plugins/spl/profile.php
+++ files/plugins/spl/profile.php
@@ -5,10 +5,10 @@ if ( $pun_config['o_spl_show_in_profile'] == '1' AND ( $pun_config['o_spl_show_g
   // Load the social-profile-links.php language file
   require PUN_ROOT.'lang/'.$pun_user['language'].'/social-profile-links.php';
 
+  $target = ( $pun_config['o_spl_link_target'] ) ? ' target="_blank"' : '';
+
   if ( $user['spl_github'] != '' )
   {
-    $target = ( $pun_config['o_spl_link_target'] ) ? ' target="_blank"' : '';
-
     $user['spl_github'] = pun_htmlspecialchars( ( $pun_config['o_censoring'] == '1' ) ? censor_words( $user['spl_github'] ) : $user['spl_github']);
     $user_personal[] = '<dt>'.$lang_spl['github'].'</dt>';
 
@@ -24,8 +24,6 @@ if ( $pun_config['o_spl_show_in_profile'] == '1' AND ( $pun_config['o_spl_show_g
 
   if ( $user['spl_facebook'] != '' )
   {
-    $target = ( $pun_config['o_spl_link_target'] ) ? ' target="_blank"' : '';
-
     $user['spl_facebook'] = pun_htmlspecialchars( ( $pun_config['o_censoring'] == '1' ) ? censor_words( $user['spl_facebook'] ) : $user['spl_facebook'] );
     $user_personal[] = '<dt>'.$lang_spl['facebook'].'</dt>';
 
@@ -41,8 +39,6 @@ if ( $pun_config['o_spl_show_in_profile'] == '1' AND ( $pun_config['o_spl_show_g
 
   if ( $user['spl_twitter'] != '' )
   {
-    $target = ( $pun_config['o_spl_link_target'] ) ? ' target="_blank"' : '';
-
     $user['spl_twitter'] = pun_htmlspecialchars( ( $pun_config['o_censoring'] == '1' ) ? censor_words($user['spl_twitter']) : $user['spl_twitter'] );
     $user_personal[] = '<dt>'.$lang_spl['twitter'].'</dt>';
 
@@ -52,14 +48,12 @@ if ( $pun_config['o_spl_show_in_profile'] == '1' AND ( $pun_config['o_spl_show_g
     }
     else
     {
-      $user_personal[] = '<dd><span class="website"><a href="https://twitter.com/'.$user['spl_twitter'].'" title="'.$lang_spl['twitter'].'"rel="nofollow"'.$target.'>'.$user['spl_twitter'].'</a></span></dd>';
+      $user_personal[] = '<dd><span class="website"><a href="https://twitter.com/'.$user['spl_twitter'].'" title="'.$lang_spl['twitter'].' "rel="nofollow"'.$target.'>'.$user['spl_twitter'].'</a></span></dd>';
     }
   }
 
   if ( $user['spl_youtube'] != '' )
   {
-    $target = ( $pun_config['o_spl_link_target'] ) ? ' target="_blank"' : '';
-
     $user['spl_youtube'] = pun_htmlspecialchars( ( $pun_config['o_censoring'] == '1' ) ? censor_words( $user['spl_youtube']) : $user['spl_youtube'] );
     $user_personal[] = '<dt>'.$lang_spl['youtube'].'</dt>';
 
@@ -69,14 +63,12 @@ if ( $pun_config['o_spl_show_in_profile'] == '1' AND ( $pun_config['o_spl_show_g
     }
     else
     {
-      $user_personal[] = '<dd><span class="website"><a href="https://youtube.com/user/'.$user['spl_youtube'].'" title="'.$lang_spl['youtube'].'"rel="nofollow"'.$target.'>'.$user['spl_youtube'].'</a></span></dd>';
+      $user_personal[] = '<dd><span class="website"><a href="https://youtube.com/user/'.$user['spl_youtube'].'" title="'.$lang_spl['youtube'].' "rel="nofollow"'.$target.'>'.$user['spl_youtube'].'</a></span></dd>';
     }
   }
 
   if ( $user['spl_googleplus'] != '' )
   {
-    $target = ( $pun_config['o_spl_link_target'] ) ? ' target="_blank"' : '';
-
     $user['spl_googleplus'] = pun_htmlspecialchars( ( $pun_config['o_censoring'] == '1' ) ? censor_words( $user['spl_googleplus'] ) : $user['spl_googleplus'] );
     $user_personal[] = '<dt>'.$lang_spl['google+'].'</dt>';
 
