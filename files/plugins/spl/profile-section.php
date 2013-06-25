111e5a1afbae3642606d3ec413cbe235cba8faff
Daniël Rokven <rokven@gmail.com>
2013-06-25 23:26:21 +0200

    Solved the undefined index

--- files/plugins/spl/profile-section.php
+++ files/plugins/spl/profile-section.php
@@ -21,32 +21,38 @@ $spl_config = unserialize( $pun_config['o_social_profile_links'] );
             <?php
             if ( $spl_config['github'] == '1' )
             {
-              echo '<label>'.$lang_spl['github'].'<br /><input id="github" type="text" name="form[github]" value="'.pun_htmlspecialchars( $spl_user['github'] ).'" size="40" maxlength="50" placeholder="'.$lang_spl['username'].'" /><br /></label>';
+              $spl_user['github'] = isset( $spl_user['github'] ) ? pun_htmlspecialchars( $spl_user['github'] ) : '';
+              echo '<label>'.$lang_spl['github'].'<br /><input id="github" type="text" name="form[github]" value="'.$spl_user['github'].'" size="40" maxlength="50" placeholder="'.$lang_spl['username'].'" /><br /></label>';
             }
 
             if ( $spl_config['facebook'] == '1' )
             {
-              echo '<label>'.$lang_spl['facebook'].'<br /><input id="facebook" type="text" name="form[facebook]" value="'.pun_htmlspecialchars( $spl_user['facebook'] ).'" size="40" maxlength="50" placeholder="'.$lang_spl['username'].'" /><br /></label>';
+              $spl_user['facebook'] = isset( $spl_user['facebook'] ) ? pun_htmlspecialchars( $spl_user['facebook'] ) : '';
+              echo '<label>'.$lang_spl['facebook'].'<br /><input id="facebook" type="text" name="form[facebook]" value="'.$spl_user['facebook'].'" size="40" maxlength="50" placeholder="'.$lang_spl['username'].'" /><br /></label>';
             }
 
             if ( $spl_config['twitter'] == '1' )
             {
-              echo '<label>'.$lang_spl['twitter'].'<br /><input id="twitter" type="text" name="form[twitter]" value="'.pun_htmlspecialchars( $spl_user['twitter'] ).'" size="40" maxlength="15" placeholder="'.$lang_spl['username'].'" /><br /></label>';
+              $spl_user['twitter'] = isset( $spl_user['twitter'] ) ? pun_htmlspecialchars( $spl_user['twitter'] ) : '';
+              echo '<label>'.$lang_spl['twitter'].'<br /><input id="twitter" type="text" name="form[twitter]" value="'.$spl_user['twitter'].'" size="40" maxlength="15" placeholder="'.$lang_spl['username'].'" /><br /></label>';
             }
 
             if ( $spl_config['youtube'] == '1' )
             {
-              echo '<label>'.$lang_spl['youtube'].'<br /><input id="youtube" type="text" name="form[youtube]" value="'.pun_htmlspecialchars( $spl_user['youtube'] ).'" size="40" maxlength="20" placeholder="'.$lang_spl['username'].'" /><br /></label>';
+              $spl_user['youtube'] = isset( $spl_user['youtube'] ) ? pun_htmlspecialchars( $spl_user['youtube'] ) : '';
+              echo '<label>'.$lang_spl['youtube'].'<br /><input id="youtube" type="text" name="form[youtube]" value="'.$spl_user['youtube'].'" size="40" maxlength="20" placeholder="'.$lang_spl['username'].'" /><br /></label>';
             }
 
             if ( $spl_config['google+'] == '1' )
             {
-              echo '<label>'.$lang_spl['google+'].'<br /><input id="google+" type="text" name="form[google+]" value="'.pun_htmlspecialchars( $spl_user['google+'] ).'" size="40" maxlength="21" placeholder="'.$lang_spl['user id'].'"/><br /></label>';
+              $spl_user['google+'] = isset( $spl_user['google+'] ) ? pun_htmlspecialchars( $spl_user['google+'] ) : '';
+              echo '<label>'.$lang_spl['google+'].'<br /><input id="google+" type="text" name="form[google+]" value="'.$spl_user['google+'].'" size="40" maxlength="21" placeholder="'.$lang_spl['user id'].'"/><br /></label>';
             }
 
             if ( $spl_config['instagram'] == '1' )
             {
-              echo '<label>'.$lang_spl['instagram'].'<br /><input id="instagram" type="text" name="form[instagram]" value="'.pun_htmlspecialchars( $spl_user['instagram'] ).'" size="40" maxlength="30" placeholder="'.$lang_spl['username'].'"/><br /></label>';
+              $spl_user['instagram'] = isset( $spl_user['instagram'] ) ? pun_htmlspecialchars( $spl_user['instagram'] ) : '';
+              echo '<label>'.$lang_spl['instagram'].'<br /><input id="instagram" type="text" name="form[instagram]" value="'.$spl_user['instagram'].'" size="40" maxlength="30" placeholder="'.$lang_spl['username'].'"/><br /></label>';
             } ?>
             </div>
           </fieldset>
